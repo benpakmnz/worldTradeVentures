0f66afca6a1359fc2f2c0b4bccd3ef2d8916dea4
benpakmnz <benpakmnz@gmail.com>
2020-08-11 10:11:41 +0300

    logos changes and javascript loading items

---
+++ data.js
@@ -0,0 +1,98 @@
+    let company=  [
+        {
+            "link": "https://www.wearshade.com/",
+            "logo": "shade.png",
+            "name": "Shade",
+            "description": "Shade has developed the first wearable UV Sensor for persons diagnosed with Lupus. Shade helps patients determine their daily threshold for UV exposure and then provides them with timely notifications so that they can make smart choices about when to go inside and when to enjoy the sunshine."
+        },
+        {
+            "link": "https://rentigo.com/",
+            "logo": "rentigo.png",
+            "name": "Rentigo",
+            "description": "Rentigo disrupts proprty management through a simplified, digitized processing, maintenance and reporting solution."
+        },
+        {
+            "link": "https://www.aquant.io/",
+            "logo": "aquant.png",
+            "name": "Aquant",
+            "slidePic": "aquant.png",
+            "description": "Aquant is an enterprise AI platform that uses machine learning to learn your enterprise's unique language and use it to maximize equipment uptime."
+        },
+        {
+            "link": "https://www.cinchwallet.com/",
+            "logo": "cinch.png",
+            "name": "Cinch",
+            "description": "Cinch is a payment app that converts customer loyalty into cash. Its mission is to help local neighborhoods thrive through local commerce."
+        },
+        {
+        "link":"https://www.juliusworks.com/", 
+        "logo":"julius.png",
+        "name": "Julius",
+        "description": "Julius is revolutionizing influencer marketing by providing clients with in depth audience demographics of social influencers making influencer marketing a science."
+        },
+        {
+        "link":"https://www.cityhive.net/", 
+        "logo":"CityHive.png",
+        "name": "City Hive’s",
+        "description": "City Hive’s all in one e-commerce platform for wine and spirit shops that enables retailers to maintain complete control over their customer base. "
+        },
+        {
+        "link":"https://www.semperis.com/", 
+        "logo":"semperis.png",
+        "name": "Semperis",
+        "slidePic": "semperis.png",
+        "description": "Semperis is an enterprise identity protection company that enables organizations to quickly recover from accidental or malicious changes and disasters that compromise Active Directory, on-premises and on cloud."
+        },
+        {
+        "link":"https://firedome.io/", 
+        "logo":"firedome.png",
+        "name": "Firedome",
+        "slidePic": "firedome.png",
+        "description": "Firedome secures and supports IoT connected devices and prevent next cyber-catastrophe. Firedome offers products and services that support different needs from active cyber-protection to analytics, insights, and cyber-security fleet management."
+        },
+        {
+        "link":"https://sqream.com/", 
+        "logo":"sqream.png",
+        "name": "SQream",
+        "slidePic": "sqream.png",
+        "description": "SQream delivers the most flexible database for huge dataset analytics. Global enterprises use SQream DB to analyze more data than ever before. "
+        },
+        {
+        "link":"https://www.nlmatics.com/", 
+        "logo":"NLMatics.png",
+        "name": "NLMatics",
+        "description": "NLMatics is focused on extracting insights from unstructured text data with minimum or no supervision. From informal notes to complex legal documents, from news headlines to extensive reports - NLMatics is designed to be flexible so that enterprises have a single solution for text analytics."
+        },
+        {
+        "link":"https://visit.org/", 
+        "logo":"visitdotorg.png",
+        "name": "Visit.org",
+        "description": "Visit.org is a SaaS platform for employee-driven corporate social responsibility (CSR). Discover & book hundreds of social impact team experiences benefitting local social ventures, and provide analytics to match these with employees' individual interests."
+        },
+        {
+        "link":"https://www.roxfinancial.com/", 
+        "logo":"rox.png",
+        "name": "ROX",
+        "description": "ROX is an exchange for real estate security tokens targeting the institutional market. It was founded in 2018 and is based in Tel Aviv, Israel and New York City, USA."
+        },
+        {
+        "link":"https://www.lili.co/", 
+        "logo":"lili.png",
+        "name": "Lili",
+        "description": "Lili empowers freelancers with financial services and tools to balance work and life in one account."
+        },
+        {
+        "link":"https://vitrainer.com/", 
+        "logo":"vi.png",
+        "name": "VI",
+        "description": "Vi delivers on-demand cardio audio workouts that are fun, effective, and adapt to you in real-time while anyone works out. Vi's mission is to democratize personal health & fitness through AI and voice"
+        },
+        {
+        "link": "https://www.igentify.com/",
+        "logo": "igentify.png",
+        "name": "igentify",
+        "description":"Founded in October 2016, Igentify’s team of globally renowned geneticists, experienced engineers, animators, and UX/UI designers are redefining the protocols of traditional medicine. In January 2019 Deloitte selected Igentify as an Israeli startup at the forefront of the Digital R&D revolution."
+        }
+
+    ]
+
