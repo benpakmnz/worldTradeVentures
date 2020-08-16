878d21c11ca6782bca94f8b2a5f2c2182b906271
benpakmnz <benpakmnz@gmail.com>
2020-08-16 13:46:41 +0300

    update data and html

---
+++ js/data.js
@@ -0,0 +1,231 @@
+const companies=  [
+        {
+            "link": "https://www.wearshade.com/",
+            "logo": "shade.png",
+            "name": "Shade",
+            "description": "Shade has developed the first wearable UV Sensor for persons diagnosed with Lupus. Shade helps patients determine their daily threshold for UV exposure and then provides them with timely notifications so that they can make smart choices about when to go inside and when to enjoy the sunshine."
+        },
+        {
+            "link": "https://rentigo.com/",
+            "logo": "rentigo.png",
+            "name": "Rentigo",
+            "description": "Rentigo disrupts proprty management through a simplified, digitized processing, maintenance and reporting solution."
+        },
+        {
+            "link": "https://www.aquant.io/",
+            "logo": "aquant.png",
+            "name": "Aquant",
+            "slidePic": "aquant.png",
+            "description": "Aquant is an enterprise AI platform that uses machine learning to learn your enterprise's unique language and use it to maximize equipment uptime."
+        },
+        {
+            "link": "https://www.cinchwallet.com/",
+            "logo": "cinch.png",
+            "name": "Cinch",
+            "description": "Cinch is a payment app that converts customer loyalty into cash. Its mission is to help local neighborhoods thrive through local commerce."
+        },
+        {
+        "link":"https://www.juliusworks.com/", 
+        "logo":"julius.png",
+        "name": "Julius",
+        "description": "Julius is revolutionizing influencer marketing by providing clients with in depth audience demographics of social influencers making influencer marketing a science."
+        },
+        {
+        "link":"https://www.cityhive.net/", 
+        "logo":"CityHive.png",
+        "name": "City Hive’s",
+        "description": "City Hive’s all in one e-commerce platform for wine and spirit shops that enables retailers to maintain complete control over their customer base. "
+        },
+        {
+        "link":"https://www.semperis.com/", 
+        "logo":"semperis.png",
+        "name": "Semperis",
+        "slidePic": "semperis.png",
+        "description": "Semperis is an enterprise identity protection company that enables organizations to quickly recover from accidental or malicious changes and disasters that compromise Active Directory, on-premises and on cloud."
+        },
+        {
+        "link":"https://firedome.io/", 
+        "logo":"firedome.png",
+        "name": "Firedome",
+        "slidePic": "firedome.png",
+        "description": "Firedome secures and supports IoT connected devices and prevent next cyber-catastrophe. Firedome offers products and services that support different needs from active cyber-protection to analytics, insights, and cyber-security fleet management."
+        },
+        {
+        "link":"https://sqream.com/", 
+        "logo":"sqream.png",
+        "name": "SQream",
+        "slidePic": "sqream.png",
+        "description": "SQream delivers the most flexible database for huge dataset analytics. Global enterprises use SQream DB to analyze more data than ever before. "
+        },
+        {
+        "link":"https://www.nlmatics.com/", 
+        "logo":"NLMatics.png",
+        "name": "NLMatics",
+        "description": "NLMatics is focused on extracting insights from unstructured text data with minimum or no supervision. From informal notes to complex legal documents, from news headlines to extensive reports - NLMatics is designed to be flexible so that enterprises have a single solution for text analytics."
+        },
+        {
+        "link":"https://visit.org/", 
+        "logo":"visitdotorg.png",
+        "name": "Visit.org",
+        "description": "Visit.org is a SaaS platform for employee-driven corporate social responsibility (CSR). Discover & book hundreds of social impact team experiences benefitting local social ventures, and provide analytics to match these with employees' individual interests."
+        },
+        {
+        "link":"https://www.roxfinancial.com/", 
+        "logo":"rox.png",
+        "name": "ROX",
+        "description": "ROX is an exchange for real estate security tokens targeting the institutional market. It was founded in 2018 and is based in Tel Aviv, Israel and New York City, USA."
+        },
+        {
+        "link":"https://www.lili.co/", 
+        "logo":"lili.png",
+        "name": "Lili",
+        "description": "Lili empowers freelancers with financial services and tools to balance work and life in one account."
+        },
+        {
+        "link":"https://vitrainer.com/", 
+        "logo":"vi.png",
+        "name": "VI",
+        "description": "Vi delivers on-demand cardio audio workouts that are fun, effective, and adapt to you in real-time while anyone works out. Vi's mission is to democratize personal health & fitness through AI and voice"
+        },
+        {
+        "link": "https://www.igentify.com/",
+        "logo": "igentify.png",
+        "name": "igentify",
+        "description":"Founded in October 2016, Igentify’s team of globally renowned geneticists, experienced engineers, animators, and UX/UI designers are redefining the protocols of traditional medicine. In January 2019 Deloitte selected Igentify as an Israeli startup at the forefront of the Digital R&D revolution."
+        }
+
+    ]
+
+
+const articles= [
+    {
+        "title": "How to start a side gig?",
+        "body": "Whether it’s to make some extra cash or to test out a new career path (while you still have the safety of a regular income), side gigs are booming in America.",
+        "date": "23 july 2020",
+        "company":"lili",
+        "link":"https://www.lili.co/blog/how-to-start-a-side-gig",
+        "size": "xl",
+        "background": "light"
+    },
+
+    {
+        "title": "THE SERVICE LEADER’S GUIDE TO WORKFORCE KPIS",
+        "body": "You can’t manage what you don’t measure. In today’s rapidly evolving service landscape, actionable insights are the key to strategic adaptation. But not every KPI or method of measurement is equal. To understand what really moves the needle for your organization’s service delivery, you first need to understand what the metrics really mean, what they don’t, and how to use that data to drive meaningful change. ",
+        "date": "14 July 2020",
+        "company":"Aquant",
+        "link":"https://www.aquant.io/blog/service-leaders-guide-workforce-kpis/",
+        "size": "md",
+        "background": "dark"
+    },
+
+    {
+        "img":"images/37.png",
+        "size": "md",
+        "background": "dark"
+    },
+
+    {
+        "title": "Guide to Dealing with Tenant Conflicts",
+        "body": "As a property manager your goal is to maintain a peaceful environment where your tenants can all get along with each other. However, this isn’t always a realistic goal. Humans’ don’t always get along with each other and conflict will arise at some point. So what do you do when two tenants are feuding? Fear no more because we have come up with a guide to help you deal with tenant conflicts. ",
+        "date": "10 dec 2019",
+        "company":"Rentigo",
+        "link":"https://rentigo.com/guide-dealing-tenant-conflicts/",
+        "size": "md",
+        "background": "light"
+    },
+
+    {
+        "title": "SQream Signs Strategic Multi-Year Multi-Million Dollar Contract with a Leading Tier-1 Mobile Operator",
+        "body": "Operator seeks to leverage critical insights from exponentially growing data to increase revenue, customer satisfaction and loyalty, as it grows its 5G base of subscribers",
+        "date": "22 july 2020",
+        "company":"SQream",
+        "link":"https://sqream.com/sqream-signs-strategic-multi-year-multi-million-dollar-contract-with-a-leading-tier-1-mobile-operator/",
+        "size": "md",
+        "background": "dark",
+        "img": "images/39.png"
+    },
+
+    {
+        "title": "[Yahoo! Finance] HYPR Announces the Launch of their New and Innovative Influencer Customer Relationship Management Tool",
+        "body": '"Powered by the ubiquitous Salesforce CRM, our platform lets you send and store outreach communications through integrated email," said the HYPR rep.',
+        "date": "16 december 2019",
+        "company":"HYPR",
+        "link":"https://sqream.com/sqream-signs-strategic-multi-year-multi-million-dollar-contract-with-a-leading-tier-1-mobile-operator/",
+        "size": "md",
+        "background": "light"
+    },
+
+    {
+        "title": "National Drink Holidays",
+        "body": "Did you know that almost every single week there is a National Drink Holiday? Here’s a list of all the important holidays that liquor retailers",
+        "date": "13 july 2020",
+        "company":"CityHive",
+        "link":"https://www.cityhive.net/blog-post/national-drink-holidays/",
+        "size": "xl",
+        "background": "light"
+    },
+
+    {
+        "img":"images/38.png",
+        "size": "md",
+        "background": "dark"
+    },
+
+    {
+        "title": "Why I’m Hanging Out With the Semperis Crew",
+        "body": "Let’s start with a little history lesson… Back in 2014, there were a series of articles calling for the dismantling and death of Active Directory (AD) for various reasons. Fast forward to 2018, and we made calls for its demise, or simply that companies should take their AD servers, throw them off the cliff and",
+        "date": "28 July 2020",
+        "company":"Semperis",
+        "link":"https://www.semperis.com/chris-roberts-semperis-crew/",
+        "size": "xxl",
+        "background": "light"
+    },
+
+    {
+        "img":"images/40.png",
+        "size": "md",
+        "background": "dark"
+    },
+
+    {
+        "title": "How to Celebrate Pride Month Safely During the COVID-19 Pandemic",
+        "body": "As the world faces the COVID-19 pandemic, this year’s Pride Month will have to look different from any Pride that came before it. It may be hard to envision the physical celebrations and community spaces of Pride in this new normal. Luckily, there are still ways to honor the lives and victories of LGBTQ+ people this month while remaining socially distanced.",
+        "date": "2020",
+        "company":"Visit.org",
+        "link":"https://rentigo.com/guide-dealing-tenant-conflicts/",
+        "size": "xxl",
+        "background": "light"
+    },
+
+    {
+        "img":"images/42.png",
+        "size": "md",
+        "background": "dark"
+    },
+
+    {
+        "title": "What can you expense as a freelancer? The E-commerce Edition",
+        "body": "You work with your hands. You sew clothes, you design jewelries or build nightstand. You go to the post office every day. You draft invoices, respond to customers’ emails, edit pictures of your products. When exactly are you supposed to organize and prepare for your taxes?! 😉 Well, that’s exactly when a Lili checking account comes in handy and why we wrote this recap of what you can count as a business expense if you run your own online store. ",
+        "date": "8 august 2020",
+        "company":"lili",
+        "link":"https://www.lili.co/blog/what-can-you-expense-as-a-freelancer-the-e-commerce-edition",
+        "size": "md",
+        "background": "light"
+    },
+
+    {
+        "title": "Firedome Welcomes IoT Industry Veteran Arsham Hatambeiki To Advisory Board",
+        "body": "Firedome is pleased to welcome Arsham Hatambeiki to our Advisory Board today. Arsham is SVP of Product and Technology at Universal Electronics, and brings over 15 years of leadership driving IoT Ecosystem and Innovation growth.",
+        "date": "27 july 2020",
+        "company":"firedome",
+        "link":"https://firedome.io/blog/firedome-welcomes-iot-industry-veteran-arsham-hatambeiki-to-advisory-board/",
+        "size": "xl",
+        "background": "dark"
+    },
+
+    {
+        "img":"images/37.png",
+        "size": "md",
+        "background": "dark"
+    },
+]
---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+$( document ).ready(function(){
+    new Glide('.glide').mount({})
+    var portfolioGlide = new Glide('#glide-portfolio', {
+    type: 'carousel',
+    perView: 4,
+    gap: 0
+    })
+    
+    portfolioGlide.on('mount.before', function(){
+      var slider = companies.filter(el => el.slidePic)
+      slider.forEach(el => {
+              var compSlide = `<li class="glide__slide">
+                      <a href="${el.link}" target="_blank" class="companie-item">
+                          <img class="companie-logo" src="images/logos/${el.logo}"/>
+                          <img class="companie-pic" src="images/${el.slidePic}"/>
+                      </a>
+                  </li>`
+                  $(compSlide).appendTo("ul.glide__slides")
+          
+      })
+    });
+    
+    portfolioGlide.mount();
+})
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
+
+companies.forEach(el=> {
+    var comp = `<li>
+                <a href="${el.link}"" target="_blank"><img src="images/logos/${el.logo}"/>
+                <div class="tooltiptext">
+                        <h3>${el.name}</h3>
+                        <p>${el.description}</p> 
+                </div>
+            </a></li>`
+            $(comp).appendTo(".companies-desc ul")
+})
+
+articles.forEach(el=> {
+    var article = ""
+    if(el.title){
+        article = `<a href= "${el.link}" target="blank" style="background-image: url(${el.img})" class="news-item ${el.size} ${el.background}">
+        <div class="item-content">
+            <h4>${el.title}</h4>
+            <p>${el.body}</p>
+        </div>
+        <div class="item-info">
+           <span>${el.date}</span>
+           <span>${el.company}</span>
+        </div>
+       </a>`
+    } else{
+        article = `<div class="news-item ${el.size} ${el.background}"><img src="${el.img}"></div>`
+    }
+
+     $(article).appendTo(".news-wraper")
+
+})
+
+
+$(".companies-desc a").hover(function() {
+var el_pos = $(this).offset().left;
+var screen_width = $(".companies-desc").width()
+var offset_width = screen_width - el_pos
+  if(offset_width < 320 ){
+  $(this).find('.tooltiptext').addClass('right');
+  }else if(offset_width > 1000){
+  $(this).find('.tooltiptext').addClass('left');
+  }
+  });
