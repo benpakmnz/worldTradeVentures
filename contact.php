122fce270a1b3403ba9e23ca60887abe525e66b0
benpakmnz <benpakmnz@gmail.com>
2020-12-25 00:10:05 +0200

    finlize project

--- contact.php
+++ contact.php
@@ -4,6 +4,7 @@ $field_position = $_POST['cf_position'];
 $field_company = $_POST['cf_company'];
 $field_website = $_POST['cf_website'];
 $field_message = $_POST['cf_message'];
+$field_email = $_POST['cf_email'];
 
 $mail_to = 'yidi@worldtradeventures.com';
 $subject = 'Message from a site visitor '.$field_name;
@@ -12,10 +13,11 @@ $body_message = 'From: '.$field_name."\n";
 $body_message .= 'position: '.$field_position."\n";
 $body_message .= 'company: '.$field_company."\n";
 $body_message .= 'website: '.$field_website."\n";
-$body_message .= 'Message: '.$field_message;
+$body_message .= 'Message: '.$field_message."\n";
+$body_message .= 'Mail: ' .$field_email;
 
 $headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
+$headers = 'Reply-To: '.$field_email."\r\n";
 
 $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
@@ -29,7 +31,7 @@ if ($mail_status) { ?>
 else { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Message failed. Please, send an email to yidi@worldtradeventures.com');
-		window.location = 'contact_page.html';
+		window.location = 'index.html';
 	</script>
 <?php
 }
